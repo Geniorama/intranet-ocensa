17d6d6c7bc15d9bd03d2f6dd45624ea3322b4c7f
DuvanCoder <duvanprogrammer@gmail.com>
2021-05-31 17:51:14 -0500

    ajustes final 31 mayo

--- src/app.js
+++ src/app.js
@@ -484,6 +484,8 @@ import "./img/estaciones/cusiana/14-icon-gestion-ambiental/plan_manejo_ambiental
 import "./img/estaciones/cusiana/14-icon-gestion-ambiental/sistema_aireacion_piscinas.svg"
 import "./img/estaciones/cusiana/line-medium.svg";
 import "./img/estaciones/cusiana/line-medium-2.svg";
+import "./img/estaciones/cusiana/chulo-verde.svg";
+import "./img/estaciones/cusiana/close-green.svg";
 
 // PERFIL ESTACIONES CASANARE
 import "./img/perfil-oleoducto-casanare/icon_perfil_ocensa_casanare.svg";
