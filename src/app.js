75c834a58d89ede2f35b78092b4649e3cb3fe01c
Geniorama <geniorama.info@gmail.com>
2021-06-19 10:48:08 -0500

    Widget grupos de trabajo

--- src/app.js
+++ src/app.js
@@ -1784,14 +1784,4 @@ $('.oc-gestion-miraflores__car').slick({
     ]
 })
 
-$('.owl-carousel-projects').owlCarousel({
-    items: 5,
-    nav: true,
-    navText: [`<svg xmlns="http://www.w3.org/2000/svg" width="6.035" height="10.071" viewBox="0 0 6.035 10.071">
-    <path id="Trazado_12569" data-name="Trazado 12569" d="M3.621,7.243.6,4.225l-.6-.6L3.621,0" transform="translate(1 1.414)" fill="none" stroke="#c9cdd2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-</svg>`,
-    
-    `<svg xmlns="http://www.w3.org/2000/svg" width="6.035" height="10.071" viewBox="0 0 6.035 10.071">
-    <path id="Trazado_12568" data-name="Trazado 12568" d="M3.621,7.243.6,4.225l-.6-.6L3.621,0" transform="translate(5.035 8.657) rotate(180)" fill="none" stroke="#c9cdd2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-</svg>`]
-})
+
