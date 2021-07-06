92474753131f8ec352677b964ff69e143c2a65ef
Geniorama <geniorama.info@gmail.com>
2021-07-06 18:07:06 -0500

    solucion-submenu-notas

--- src/app.js
+++ src/app.js
@@ -1764,23 +1764,6 @@ $('.wdg-grupos__car').slick({
     slidesToScroll: 3,
 })
 
-// $('.oc-members-carousel').slick({
-//     arrows: true,
-//     dots: false,
-//     slidesToScroll: 1,
-//     slidesToShow: 5,
-//     prevArrow: `<button class='oc-slick-arrow oc-slick-arrow__prev'>
-//                 <svg xmlns="http://www.w3.org/2000/svg" width="6.035" height="10.071" viewBox="0 0 6.035 10.071">
-//                     <path id="Trazado_12569" data-name="Trazado 12569" d="M3.621,7.243.6,4.225l-.6-.6L3.621,0" transform="translate(1 1.414)" fill="none" stroke="#c9cdd2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-//                 </svg>
-//     </button>`,
-//     nextArrow: `<button class='oc-slick-arrow oc-slick-arrow__next'>
-//         <svg xmlns="http://www.w3.org/2000/svg" width="6.035" height="10.071" viewBox="0 0 6.035 10.071">
-//             <path id="Trazado_12568" data-name="Trazado 12568" d="M3.621,7.243.6,4.225l-.6-.6L3.621,0" transform="translate(5.035 8.657) rotate(180)" fill="none" stroke="#c9cdd2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-//         </svg>
-//     </button>`,
-// })
-
 $('.oc-members-carousel-02').slick({
     arrows: true,
     dots: false,
