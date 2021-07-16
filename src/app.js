c30470139ca6521d6db43abc4121420b878998fa
Geniorama <geniorama.info@gmail.com>
2021-07-15 23:57:46 -0500

    Avance 2 la granjita

--- src/app.js
+++ src/app.js
@@ -1045,6 +1045,12 @@ import "./img/cordoba/estacion-la-granjita/LINKS/img/slide_1_granjita.jpg";
 import "./img/cordoba/estacion-la-granjita/LINKS/img/slide_2_granjita.jpg";
 import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/1-icon-la-estacion/la_estacion_proyecto_135.svg";
 import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/1-icon-la-estacion/la_estacion_esquema_de_bombeo.svg";
+import "./img/cordoba/estacion-la-granjita/LINKS/img/ubicacion-la-granjita.jpg";
+import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/12-icon-bombeo/turbinas-2.svg";
+import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/12-icon-bombeo/line-turbinas-2.svg";
+import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/12-icon-bombeo/bombeo-grap-1.svg";
+import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/12-icon-bombeo/bombeo-grap-2.svg";
+import "./img/cordoba/estacion-la-granjita/LINKS/svg editables/12-icon-bombeo/bombas-granjita.svg";
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
@@ -2147,3 +2153,40 @@ $('.oc-vasc-almacenamoiento__car').slick({
     ]
 })
 
+
+
+// Slick Almacenamiento La Granjita
+$('.oc-slick-almacenamiento-granjita').slick({
+    slidesToShow: 2,
+    prevArrow: `<button class='oc-slick-arrow oc-slick-arrow__prev'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="21.871" height="21.871" viewBox="0 0 21.871 21.871">
+            <g id="Componente_85" data-name="Componente 85" transform="translate(0.392 0.392)">
+            <g id="Grupo_13271" data-name="Grupo 13271">
+                <circle id="Elipse_2235" data-name="Elipse 2235" cx="10.543" cy="10.543" r="10.543" transform="translate(0)" fill="#fff" stroke="#505d6c" stroke-miterlimit="10" stroke-width="0.785"/>
+                <path id="Trazado_11719" data-name="Trazado 11719" d="M4.075,8.151,0,4.075,4.075,0" transform="translate(7.858 6.468)" fill="none" stroke="#505d6c" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.355"/>
+            </g>
+            </g>
+        </svg>
+    </button>`,
+    nextArrow: `<button class='oc-slick-arrow oc-slick-arrow__next'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="21.871" height="21.871" viewBox="0 0 21.871 21.871">
+            <g id="Componente_84" data-name="Componente 84" transform="translate(21.479 21.479) rotate(180)">
+            <g id="Grupo_13272" data-name="Grupo 13272">
+                <circle id="Elipse_2236" data-name="Elipse 2236" cx="10.543" cy="10.543" r="10.543" transform="translate(0)" fill="none" stroke="#505d6c" stroke-miterlimit="10" stroke-width="0.785"/>
+                <path id="Trazado_11720" data-name="Trazado 11720" d="M4.075,8.151,0,4.075,4.075,0" transform="translate(7.858 6.468)" fill="none" stroke="#505d6c" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.355"/>
+            </g>
+            </g>
+        </svg>
+    </button>`,
+    dots: true,
+    arrows: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+              dots: true,
+              slidesToShow: 1
+            }
+        }
+    ]
+})
