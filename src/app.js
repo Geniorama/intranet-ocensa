b3d70ceecaa9eaf3375d357d30bf729515c0ccb0
Geniorama <geniorama.info@gmail.com>
2021-09-04 19:12:48 -0500

    Tablet est coveñas

--- src/app.js
+++ src/app.js
@@ -1388,7 +1388,7 @@ $('.oc-slick-widget__alm-covenas').slick({
     </button>`,
     responsive: [
         {
-            breakpoint: 768,
+            breakpoint: 769,
             settings:{
                 slidesToShow: 1,
                 dots: true
